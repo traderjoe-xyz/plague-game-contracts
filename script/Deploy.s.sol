54fdfd1e835083317a79bc6356c0e88f61d92b4a
Patate <patate@joepegs.com>
2022-11-17 21:33:18 +0100

    remove last villagers mentions

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -15,7 +15,7 @@ contract DeployScript is Script {
         [2_000, 2_000, 2_000, 3_000, 3_000, 3_000, 4_000, 4_000, 4_000, 5_000, 5_000, 5_000];
 
     // Fuji
-    ERC721Mock villagers = ERC721Mock(0x24c60BFC0572f855f3F5289fca67fc26a7E7E5f1);
+    ERC721Mock doctors = ERC721Mock(0x24c60BFC0572f855f3F5289fca67fc26a7E7E5f1);
     ERC721Mock potions = ERC721Mock(0xeDD3DDA6dc38514809bb0F187dC3166902c293DB);
     VRFCoordinatorV2Interface coordinator = VRFCoordinatorV2Interface(0x2eD832Ba664535e5886b75D64C46EB9a228C2610);
     uint64 subscriptionId = 139;
@@ -23,7 +23,7 @@ contract DeployScript is Script {
     uint32 maxGas = 800_000;
 
     // Mainnet
-    ERC721Mock villagers_mainnet = ERC721Mock(0x6a0e6AA00A85d544f2F6bC6D940aDFBd4FD8b4b9);
+    ERC721Mock doctors_mainnet = ERC721Mock(0x6a0e6AA00A85d544f2F6bC6D940aDFBd4FD8b4b9);
     ERC721Mock potions_mainnet = ERC721Mock(0xd7C046164bd7D84Db761bD9F97A5AF8D16B93332);
     VRFCoordinatorV2Interface coordinator_mainnet =
         VRFCoordinatorV2Interface(0x2eD832Ba664535e5886b75D64C46EB9a228C2610);
@@ -35,7 +35,7 @@ contract DeployScript is Script {
         if (block.chainid == 43114) {
             console.log("Deploying on Avalanche Mainnet");
 
-            villagers = villagers_mainnet;
+            doctors = doctors_mainnet;
             potions = potions_mainnet;
             coordinator = coordinator_mainnet;
             subscriptionId = subscriptionId_mainnet;
@@ -47,7 +47,7 @@ contract DeployScript is Script {
 
         vm.startBroadcast(deployerPrivateKey);
         PlagueGame plagueGame = new PlagueGame(
-            villagers,
+            doctors,
             potions,
             block.timestamp + 120,
             playerNumberToEndGame,
